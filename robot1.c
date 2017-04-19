4a7fdaac8b9fdaf7ac061eb0d024406200eece4e
ikami1 <chicken3579@o2.pl>
2017-04-19 21:44:00 +0200

    Add files via upload

--- robot1.c
+++ robot1.c
@@ -8,6 +8,7 @@ uint16_t distance;
 int main(void){
 	DDRB |= (1<<PB1)|(1<<PB2);	//ustawienie PB1 i PB2 jako wyjscie
     DDRB |= (0<<PB0);   //ustawienie PB0 jako wejscie
+	DDRD |= (1<<PD7);
 
     TCCR1B |= (1<<ICES1);   //rosnace zbocze wyzwala przerwania
     TCCR1B |= (1<<CS10);    //no prescaler, 1 cykl zegara to 1 mikrosekunda (przy 1 MHz)
@@ -29,6 +30,10 @@ ISR(TIMER1_CAPT_vect){  //przerwanie na PB0
     if( !(TCCR1B & (1<<ICES1)) ){
         distance = ICR1 - LastCapture;      //dlugosc zbocza
         distance = distance/58;             //odleglosc w cm    (dla uint16_t odleglosc max 11m)
+		if(distance > 50)
+			PB1 = 1;
+		if(distance > 100)
+			PD7 = 1;
     }
     LastCapture = ICR1;
 
